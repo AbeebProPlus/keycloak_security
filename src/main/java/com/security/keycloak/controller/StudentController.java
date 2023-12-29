f8540c19e50086d1a9291200ea44485a13da25f4
abeeb ahmad <mdahlat94@gmail.com>
2023-12-29 15:15:09 +0100

    keycloak 2

--- src/main/java/com/security/keycloak/controller/StudentController.java
+++ src/main/java/com/security/keycloak/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.security.keycloak.controller;
 import com.security.keycloak.dto.StudentDto;
 import com.security.keycloak.service.StudentService;
 import com.security.keycloak.model.Student;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,17 +11,17 @@ import org.springframework.web.bind.annotation.*;
 public class StudentController {
     private final StudentService studentService;
 
-    public StudentController(StudentService studentService){
+    public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
     @PostMapping("/add")
-    public Student addStudent(@RequestBody StudentDto studentDto){
+    public Student addStudent(@RequestBody StudentDto studentDto) {
         return studentService.addStudent(studentDto);
     }
 
     @GetMapping("/{name}")
-    public Student getStudentByName(@PathVariable String name){
+    public Student getStudentByName(@PathVariable String name) {
         return studentService.findByName(name);
     }
 
