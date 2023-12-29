9775055fffc9886b7a6e4c161287c35764841def
abeeb ahmad <mdahlat94@gmail.com>
2023-12-29 09:35:52 +0100

    keycloak 2

--- src/main/java/com/security/keycloak/security/SecurityConfig.java
+++ src/main/java/com/security/keycloak/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.security.keycloak.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,9 +16,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
-        httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
+        httpSecurity.authorizeHttpRequests(authorize ->
+                authorize
+                        .requestMatchers(HttpMethod.GET, "/student/{name}").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/student/add").permitAll()
+                        .anyRequest().authenticated()
+        );
         httpSecurity.oauth2ResourceServer(oauth2 -> {
-            oauth2.opaqueToken(Customizer.withDefaults());
+            oauth2.jwt(Customizer.withDefaults());
         });
         httpSecurity.sessionManagement(session -> {
             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
