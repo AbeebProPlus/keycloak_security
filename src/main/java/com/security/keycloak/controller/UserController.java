7de94188c77cb624c2960eeea8918e1a067d769a
AbeebProPlus <mdahlat94@gmail.com>
2024-01-20 17:25:34 +0100

    keycloak

--- src/main/java/com/security/keycloak/controller/UserController.java
+++ src/main/java/com/security/keycloak/controller/UserController.java
@@ -1,39 +1,39 @@
-package com.security.keycloak.controller;
-
-import com.security.keycloak.dto.AppUserDto;
-import com.security.keycloak.dto.UpdateUserNameDto;
-import com.security.keycloak.dto.UserDto;
-import com.security.keycloak.service.UserService;
-import com.security.keycloak.model.User;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/user")
-@SecurityRequirement(name = "keycloak")
-public class UserController {
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @PostMapping("/add")
-//    @PreAuthorize("hasRole('user')")
-    public User addUser(@RequestBody AppUserDto userDto) {
-        return userService.addUser(userDto);
-    }
-
-    @GetMapping("/{name}")
-//    @PreAuthorize("hasRole('flyer')")
-    public User getUserByUserName(@PathVariable String name) {
-        return userService.findByUserName(name);
-    }
-
-    @PutMapping("/update")
-    public User updateUserName(@RequestBody UpdateUserNameDto updateUserNameDto){
-        return userService.editUserName(updateUserNameDto);
-    }
-
-}
+//package com.security.keycloak.controller;
+//
+//import com.security.keycloak.dto.AppUserDto;
+//import com.security.keycloak.dto.UpdateUserNameDto;
+//import com.security.keycloak.dto.UserDto;
+//import com.security.keycloak.service.UserService;
+//import com.security.keycloak.model.User;
+//import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/user")
+//@SecurityRequirement(name = "keycloak")
+//public class UserController {
+//    private final UserService userService;
+//
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @PostMapping("/add")
+////    @PreAuthorize("hasRole('user')")
+//    public User addUser(@RequestBody AppUserDto userDto) {
+//        return userService.addUser(userDto);
+//    }
+//
+//    @GetMapping("/{name}")
+////    @PreAuthorize("hasRole('flyer')")
+//    public User getUserByUserName(@PathVariable String name) {
+//        return userService.findByUserName(name);
+//    }
+//
+//    @PutMapping("/update")
+//    public User updateUserName(@RequestBody UpdateUserNameDto updateUserNameDto){
+//        return userService.editUserName(updateUserNameDto);
+//    }
+//
+//}
